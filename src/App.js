addde0d57aa9de60f2b2de45e3d2f269437d3287
Song Seungeun <grayandjj@gmail.com>
2020-08-05 01:11:36 +0900

    create basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CurrentWeather from "./Components/CurrentWeather";
+import HourWeather from "./Components/HourWeather";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CurrentWeather />
+      <HourWeather />
+    </>
   );
 }
 
---
+++ src/Components/HourWeather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HourWeather = () => {
+  return <div>HourWeather</div>;
+};
+
+export default HourWeather;
